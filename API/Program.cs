02d113e5acd25c2e151d2e86407c5e3419fa91a6
mansourlotfi <mansourlotfi@gmail.com>
2020-03-08 17:14:49 +0330

    backend crud operation

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -22,8 +18,9 @@ namespace API
                 var Services = scop.ServiceProvider;
                 try
                 {
-                    var contxt = Services.GetRequiredService<DataContext>();
-                    contxt.Database.Migrate();
+                    var context = Services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                    Seed.SeedData(context);
                 }
                 catch (Exception ex)
                 {
