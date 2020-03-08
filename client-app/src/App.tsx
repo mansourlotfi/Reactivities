d831691b96b99202b9324e6eb9eb90229afbb8e6
mansourlotfi <mansourlotfi@gmail.com>
2020-03-08 12:43:05 +0330

    using semantic ui and axios to display backend data

---
+++ client-app/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Header, Icon, List } from 'semantic-ui-react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+	state = {
+		Values: []
+	};
+
+	componentDidMount() {
+		axios.get('http://localhost:5000/api/values').then((response) => {
+			this.setState({
+				Values: response.data
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<Header as="h2">
+					<Icon name="users" />
+					<Header.Content>Reactivities</Header.Content>
+				</Header>
+				<List>{this.state.Values.map((value: any) => <List.Item key={value.id}>{value.name}</List.Item>)}</List>
+			</div>
+		);
+	}
+}
+
+export default App;
